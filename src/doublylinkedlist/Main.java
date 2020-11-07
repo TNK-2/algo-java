c4d1b5de130bdf9937c0a51c8b712e0d03b24481
ishizukayusuke <ishizukayusuke@team-lab.com>
2020-11-08 07:53:36 +0900

    双方向リスト

--- src/doublylinkedlist/Main.java
+++ src/doublylinkedlist/Main.java
@@ -8,9 +8,16 @@ public class Main {
         list.append('b');
         list.append('c');
         list.insert('d');
+        list.append('e');
         list.print();
-        System.out.println("cを削除します。結果は以下です。");
-        list.remove('c');
+        System.out.println("eを削除します。結果は以下です。");
+        list.remove('e');
+        list.print();
+        System.out.println("dを削除します。結果は以下です。");
+        list.remove('d');
+        list.print();
+        System.out.println("bを削除します。結果は以下です。");
+        list.remove('b');
         list.print();
     }
 }
